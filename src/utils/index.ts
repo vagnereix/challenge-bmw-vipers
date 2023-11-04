88c71b35a6a69d2445f02fbc0cff798396bbf40f
Vagmer Reis <vagnereix.dev@gmail.com>
2023-11-04 12:40:48 -0300

    feat: configuring prisma and functions utils

---
+++ src/utils/index.ts
@@ -0,0 +1,17 @@
+import { prisma } from '@/service/prisma';
+
+export async function getCustomerOrders(id: string) {
+  return await prisma.order.findMany({
+    where: {
+      customerId: id,
+    },
+  });
+}
+
+export async function getCustomer(email: string) {
+  return await prisma.customer.findUnique({
+    where: {
+      email,
+    },
+  });
+}
