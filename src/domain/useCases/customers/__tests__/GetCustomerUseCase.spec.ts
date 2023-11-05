de8865ed6a9184d8e74b99220da0639ff0791137
Vagmer Reis <vagnereix.dev@gmail.com>
2023-11-05 10:19:14 -0300

    test: GetCustomerUseCase unit tests

---
+++ src/domain/useCases/customers/__tests__/GetCustomerUseCase.spec.ts
@@ -0,0 +1,35 @@
+import { GetCustomerUseCase } from '../GetCustomerUseCase';
+
+const getCustomerSuccessSpy = jest.fn().mockResolvedValue({
+  id: 'valid-id',
+  name: 'Vagner',
+  email: 'vagnereix.dev@gmail.com',
+});
+
+const getCustomerErrorSpy = jest.fn();
+
+function makeSut(getCustomerSpy: jest.Mock) {
+  const sut = new GetCustomerUseCase({
+    create: jest.fn(),
+    getCustomerByEmail: jest.fn().mockImplementation(getCustomerSpy),
+    getCustomerOrders: jest.fn(),
+  });
+
+  return { sut };
+}
+
+describe('GetCustomerUseCase', () => {
+  it('should be able to get customer', async () => {
+    const { sut } = makeSut(getCustomerSuccessSpy);
+
+    await expect(sut.execute('vagnereix.dev@gmail.com')).resolves.not.toThrow();
+
+    expect(getCustomerSuccessSpy).toHaveBeenCalled();
+  });
+
+  it('should not be able to get customer when is a invalid email', async () => {
+    const { sut } = makeSut(getCustomerErrorSpy);
+
+    await expect(sut.execute('invalid.email@gmail.com')).rejects.toThrow();
+  });
+});
