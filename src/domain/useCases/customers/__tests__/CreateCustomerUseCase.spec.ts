9effe3eabe4105500fae6a45fce0f8b58faea5c9
Vagmer Reis <vagnereix.dev@gmail.com>
2023-11-05 10:18:38 -0300

    test: CreateCustomerUseCase unit tests

---
+++ src/domain/useCases/customers/__tests__/CreateCustomerUseCase.spec.ts
@@ -0,0 +1,51 @@
+import { CreateCustomerUseCase } from '../CreateCustomerUseCase';
+
+const getCustomerSuccessSpy = jest.fn().mockResolvedValue({
+  id: 'valid-id',
+  name: 'User',
+  email: 'used.email@gmail.com',
+});
+
+const createCustomerSuccessSpy = jest.fn().mockResolvedValue({
+  id: 'valid-id',
+  name: 'Vagner',
+  email: 'vagnereix.dev@gmail.com',
+});
+
+const createCustomerErrorSpy = jest.fn().mockRejectedValue(null);
+
+function makeSut(createCustomerSpy: jest.Mock, getCustomerSpy?: jest.Mock) {
+  const sut = new CreateCustomerUseCase({
+    create: jest.fn().mockImplementation(createCustomerSpy),
+    getCustomerByEmail: jest.fn().mockImplementation(getCustomerSpy),
+    getCustomerOrders: jest.fn(),
+  });
+
+  return { sut };
+}
+
+describe('GetCustomerUseCase', () => {
+  it('should be able to create a new customer', async () => {
+    const { sut } = makeSut(createCustomerSuccessSpy);
+
+    await expect(
+      sut.execute({
+        name: 'Vagner',
+        email: 'vagnereix.dev@gmail.com',
+      }),
+    ).resolves.not.toThrow();
+
+    expect(createCustomerSuccessSpy).toHaveBeenCalled();
+  });
+
+  it('should not be able to create a new customer with a used email', async () => {
+    const { sut } = makeSut(createCustomerErrorSpy, getCustomerSuccessSpy);
+
+    await expect(
+      sut.execute({
+        name: 'Vagner',
+        email: 'used.email@gmail.com',
+      }),
+    ).rejects.toThrow();
+  });
+});
